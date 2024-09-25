3a98952a74a98887c8220282dde09dd8b22199fb
Juan <juanmavd21@gmail.com>
2024-09-25 09:57:51 -0300

    FirstCommit

---
+++ calculadora.test.js
@@ -0,0 +1,29 @@
+const Calculadora = require('./calculadora');
+
+describe('Calculadora', () => {
+  let calc;
+
+  beforeEach(() => {
+    calc = new Calculadora();
+  });
+
+  test('sumar 2 + 3 debe ser 5', () => {
+    expect(calc.sumar(2, 3)).toBe(5);
+  });
+
+  test('restar 5 - 3 debe ser 2', () => {
+    expect(calc.restar(5, 3)).toBe(2);
+  });
+
+  test('multiplicar 4 * 3 debe ser 12', () => {
+    expect(calc.multiplicar(4, 3)).toBe(12);
+  });
+
+  test('dividir 10 / 2 debe ser 5', () => {
+    expect(calc.dividir(10, 2)).toBe(5);
+  });
+
+  test('dividir por cero debe lanzar un error', () => {
+    expect(() => calc.dividir(10, 0)).toThrow('No se puede dividir por cero');
+  });
+});
---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+export default [
+  {
+    ignores: ['node_modules/**'], // Ignora la carpeta node_modules
+  },
+  {
+    files: ['**/*.js'], // Aplica configuración a archivos .js
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'off',
+    },
+  },
+];
